ab3b730ba49fe3cfa010e749f112b57b1c57615c
prprAbukuma <37796225+prprAbukuma@users.noreply.github.com>
2019-04-09 21:28:08 +0800

    优化json生成方式
    利用org.json中的JsonObject.put函数构造json，舍弃以前手写json

--- src/main/java/Proto/GetSecurityCodeResponseProto.java
+++ src/main/java/Proto/GetSecurityCodeResponseProto.java
@@ -15,9 +15,12 @@ public class GetSecurityCodeResponseProto   {
      * @return
      */
     public static String packJsonData(GetSecurityCodeResponseProto responseProto){
-        // 生成json字符串.手动写
-        String jsonData="{ \"returnType\":\""+responseProto.returnType.ordinal()+"\",\"tipMsg\":\""+responseProto.tipMsg+"\"}";
-
-        return jsonData;
+       // 生成json字符串.手动写
+        //String jsonData="{ \"returnType\":\""+responseProto.returnType.ordinal()+"\",\"tipMsg\":\""+responseProto.tipMsg+"\"}";
+        //不手动写，用org.json自动生成json字符串
+        JSONObject jsonData=new JSONObject();//创建JSONObject对象
+        jsonData.put("returnType",responseProto.returnType.ordinal());//向其中put键值对即可
+        jsonData.put("tipMsg",responseProto.tipMsg);
+        return jsonData.toString();
     }
 }
